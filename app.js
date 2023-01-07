1526ccfdb60d5f58d0ee7ddc9101d651e412d638
Masterkent11 <clementirkent1999@gmail.com>
2023-01-07 16:39:58 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+const calculator = document.querySelector(".calculator");
+
+
+
+// Declaring the button
+
+buttons.forEach((item) => {
+  
+    // Active the Button
+    item.onclick = () => {
+    
+    if (item.id == "clear") {
+      display.innerText = "";
+    }
+
+    else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    }
+    else if (display.innerText  != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } 
+    else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "";
+    } 
+    else {
+      display.innerText += item.id;
+    
+    
+    }
+  
+};
+
+}
+);
+
+
